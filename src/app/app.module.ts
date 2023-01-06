50c2f9a71d76a1f3af76d8de6007593fa0e6ae9d
Juan Molano <juan.estebanmolano@swisscom.com>
2023-01-06 09:27:33 +0100

    try to create a movie

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,42 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { TextcomponentComponent } from './textcomponent/textcomponent.component';
+import { ImagecomponentComponent } from './imagecomponent/imagecomponent.component';
+import { AddIssueComponent } from './components/add-issue/add-issue.component';
+import { EditIssueComponent } from './components/edit-issue/edit-issue.component';
+import { IssueListComponent } from './components/issue-list/issue-list.component';
+import { BugService } from './shared/bug.service';
 
+import { HttpClientModule } from '@angular/common/http';
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'add-issue' },
+  { path: 'add-issue', component: AddIssueComponent },
+  { path: 'edit-issue/:id', component: EditIssueComponent },
+  { path: 'issues-list', component: IssueListComponent },
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TextcomponentComponent,
+    ImagecomponentComponent,
+    AddIssueComponent,
+    EditIssueComponent,
+    IssueListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  exports: [RouterModule],
+  providers: [BugService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
