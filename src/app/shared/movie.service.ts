868a4a9de90f1351bdcd4d0b36889b6239046e9f
Juan Molano <juan.estebanmolano@swisscom.com>
2023-01-06 12:07:26 +0100

    adding ng build to checks

--- src/app/shared/movie.service.ts
+++ src/app/shared/movie.service.ts
@@ -17,7 +17,6 @@ export class MovieService {
   httpOptions = {
     headers: new HttpHeaders({
       'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': 'http://localhost:4200',
     }),
   };
 
